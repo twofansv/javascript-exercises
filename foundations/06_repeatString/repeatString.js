b52246fe49417b4837ac4b2e26f816691566ba1b
twofansv <95734976+twofansv@users.noreply.github.com>
2026-02-20 10:40:03 +0800

    [+] Completed 06_repeatString exercise

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
+    if (times < 0) return "ERROR";
+
+    let result = "";
+
+    for (let i = 0; i < times; i++){
+        word += result;
+    }
+    return word;
 
 };
 
