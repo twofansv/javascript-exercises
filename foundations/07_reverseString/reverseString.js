8359f9489c07ffb3c18b6d7ab55d92711e38dd0d
twofansv <95734976+twofansv@users.noreply.github.com>
2026-02-23 00:01:57 +0800

    [^] Completed first test case.

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,4 +1,17 @@
-const reverseString = function() {
+const reverseString = function(str) {
+
+
+let reversed = "";
+
+for (let i = str.length -1; i >= 0; i--){
+    
+ 
+    reversed += str[i] ;
+    
+}
+
+return reversed;
+ 
 
 };
 
