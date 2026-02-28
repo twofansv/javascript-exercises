aeea7c367bcdfb70caaaf05381bebc7eb57ae094
twofansv <95734976+twofansv@users.noreply.github.com>
2026-02-28 20:29:35 +0800

    Completed removeFromArray exercise.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...num) {
+
+  return arr.filter(item => !num.includes(item));
+
+   
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
