e11c2cc41ce99b88d0d69b6d679b9d3b52f8d30d
twofansv <95734976+twofansv@users.noreply.github.com>
2026-02-28 21:53:03 +0800

    [+] Completed first test

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(num, num2) {
 
+
+    let sum;
+
+    for (let i = 1; i < num2; i++){
+
+        if (num <= num2) {
+            num++;
+            
+
+        }
+
+    }
+    sum = num;
+    return sum + num2;
 };
 
 // Do not edit below this line
