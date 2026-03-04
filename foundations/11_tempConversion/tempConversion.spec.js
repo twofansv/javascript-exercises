729d80288a9353087df10f05edeacb0f5a4ce891
twofansv <95734976+twofansv@users.noreply.github.com>
2026-03-04 12:14:52 +0800

    [^] Completed test exercises.

--- foundations/11_tempConversion/tempConversion.spec.js
+++ foundations/11_tempConversion/tempConversion.spec.js
@@ -16,10 +16,10 @@ describe('convertToFahrenheit', () => {
   test('works', () => {
     expect(convertToFahrenheit(0)).toEqual(32);
   });
-  test.skip('rounds to 1 decimal', () => {
+  test('rounds to 1 decimal', () => {
     expect(convertToFahrenheit(73.2)).toEqual(163.8);
   });
-  test.skip('works with negatives', () => {
+  test('works with negatives', () => {
     expect(convertToFahrenheit(-10)).toEqual(14);
   });
 });
