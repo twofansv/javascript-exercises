e5a2bdc3c872afd4d9fc029233f204079bdc590f
twofansv <95734976+twofansv@users.noreply.github.com>
2026-04-14 14:07:42 +0800

    [^] Completed this exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,20 +1,29 @@
 const fibonacci = function(num) {
-    
-    let firstNum = 0;
-    let secondNum = 1;
+
+    num = +num
+    let febSeq;
+    let firstNum = 1;
+    let secondNum = 0;
+
+if (num === 0){
+    return 0;
+}
+else if (num === 1){
+    return 1;
+}
+else if (num < 0) {
+    return 'OOPS'
+}
 
 
-    for (let i = 0; i <= num; i++) {
-        let sequence += firstNum + secondNum;
+for (let i = 2; i <= num; i++) {
+    febSeq = firstNum + secondNum; // F = 1 + 0 = 1 // F1 = 1 + 1 = 2 // F2 = 2 + 1 = 3 // --F3 = 3 + 2 = 5 
+    secondNum = firstNum; // 2 // 
+    firstNum = febSeq; // 3 // 
+}
 
-
-
-    };
-    
-    return num;
-
-
-
+return febSeq;
+ 
 };
 
 // Do not edit below this line
