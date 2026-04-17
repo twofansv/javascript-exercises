e95c5b249c3fdccad2443efaca5403b5a0068dc3
twofansv <95734976+twofansv@users.noreply.github.com>
2026-04-17 19:18:51 +0800

    [^] Completed this exercise

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
+const getAge = function(death, birth) {
+        
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+        return death - birth;
+}
+
+const findTheOldest = function (people) {
+
+
+  return people.reduce((oldest, currentPerson) => {
+    const getOldest = getAge(oldest.yearOfDeath, oldest.yearOfBirth);
+    const getCurrentPerson = getAge(currentPerson.yearOfDeath, currentPerson.yearOfBirth);
+
+    return getCurrentPerson > getOldest ? currentPerson : oldest;
+
+  });
+
+
+
 
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
--- foundations/16_findTheOldest/solution/findTheOldest-solution.js
+++ foundations/16_findTheOldest/solution/findTheOldest-solution.js
@@ -1,21 +1,23 @@
-const getAge = function (birth, death) {
-  if (!death) {
-    death = new Date().getFullYear();
-  }
-  return death - birth;
-};
+const getAge = function(death, birth) {
+        
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+        return death - birth;
+}
 
 const findTheOldest = function (people) {
-  return people.reduce((oldest, currentPerson) => {
-    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
-    const currentAge = getAge(
-      currentPerson.yearOfBirth,
-      currentPerson.yearOfDeath
-    );
-    return oldestAge < currentAge ? currentPerson : oldest;
-  });
-};
 
+
+  return people.reduce((oldest, currentPerson) => {
+    const oldest = getAge(oldest.yearOfDeath, oldest.yearOfBirth);
+    const currentPerson = getAge(currentPerson.yearOfDeath, currentPerson.yearOfBirth);
+
+    return oldest > currentPerson ? oldest : currentPerson;
+
+  });
+
+}
 /* ALTERNATIVE SOLUTION
 const getAge = function (person) {
   // The nullish coalescing assignment operator
